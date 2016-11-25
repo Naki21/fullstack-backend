4058d86789d0d334b316415250ee4d461774f467
Naki21 <Yianni21@gmail.com>
2016-11-25 15:45:02 -0500

    Sets up front end with sign in buttons to test api routes

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,44 +1,50 @@
 'use strict';
+const showSignInTemplate = require('../templates/show-sign-in');
 
 const getFormFields = require(`../../../lib/get-form-fields`);
 const store = require('../store');
 const api = require('./api');
 const ui = require('./ui');
 
-const onSignUp = function (event) {
+const onSignUp = function(event) {
   let data = getFormFields(this);
   event.preventDefault();
   api.signUp(data)
     .then(ui.success)
     .catch(ui.failure);
-    $('#sign-up-modal').modal('hide');
+  $('#sign-up-modal').modal('hide');
 };
 
-const onSignIn = function (event) {
+const onSignIn = function(event) {
   let data = getFormFields(this);
   event.preventDefault();
   api.signIn(data)
     .then(ui.signInSuccess)
     .catch(ui.failure);
-    $('#sign-in-modal').modal('hide');
+  $('#sign-in-modal').modal('hide');
 };
 
-const onChangePassword = function (event) {
+const onChangePassword = function(event) {
   let data = getFormFields(this);
   event.preventDefault();
-    api.changePassword(data)
+  api.changePassword(data)
     .then(ui.success)
     .catch(ui.failure);
 };
 //There is a better way to log out individual player
-const onSignOut = function (event){
+const onSignOut = function(event) {
   event.preventDefault();
   api.signOut(store.user)
-  .then(ui.success)
-  .catch(ui.failure);
+    .then(ui.success)
+    .catch(ui.failure);
+};
+const showSignIn = () => {
+  $('.target').html(showSignInTemplate());
+  console.log("yoyoyoy");
 };
 
 const addHandlers = () => {
+  $('#sign-in-button').on('submit', showSignIn);
   $('.sign-up-form').on('submit', onSignUp);
   $('.sign-in-form').on('submit', onSignIn);
   $('.change-password-form').on('submit', onChangePassword);
