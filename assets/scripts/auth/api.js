97f1616e8051f46ccf964a76790924f2bc66b495
Naki21 <Yianni21@gmail.com>
2016-11-25 15:53:34 -0500

    Switching to new branch

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,43 +1,43 @@
-// 'use strict';
-// const config = require('../config.js');
-// const store = require('../store');
-//
-// const signUp = (data) =>
-// $.ajax({
-//   url: config.host + "/sign-up",
-//   method: 'POST',
-//   data,
-// });
-//
-// const signIn = (data) =>
-// $.ajax({
-//   url: config.host + "/sign-in",
-//   method: 'POST',
-//   data,
-// });
-//
-// const changePassword = (data) =>
-// $.ajax({
-//   url: config.host + "/change-password/" + store.user.id,
-//   method: 'PATCH',
-//   data,
-//   headers: {
-//     Authorization: 'Token token=' + store.user.token,
-//   },
-// });
-//
-// const signOut = (player) =>
-// $.ajax({
-//   url: config.host + "/sign-out/" + player.id,
-//   method: 'DELETE',
-//   headers: {
-//     Authorization: 'Token token=' + player.token,
-//   }
-// });
-//
-// module.exports = {
-//   signUp,
-//   signIn,
-//   signOut,
-//   changePassword,
-// };
+'use strict';
+const config = require('../config.js');
+const store = require('../store');
+
+const signUp = (data) =>
+$.ajax({
+  url: config.host + "/sign-up",
+  method: 'POST',
+  data,
+});
+
+const signIn = (data) =>
+$.ajax({
+  url: config.host + "/sign-in",
+  method: 'POST',
+  data,
+});
+
+const changePassword = (data) =>
+$.ajax({
+  url: config.host + "/change-password/" + store.user.id,
+  method: 'PATCH',
+  data,
+  headers: {
+    Authorization: 'Token token=' + store.user.token,
+  },
+});
+
+const signOut = (player) =>
+$.ajax({
+  url: config.host + "/sign-out/" + player.id,
+  method: 'DELETE',
+  headers: {
+    Authorization: 'Token token=' + player.token,
+  }
+});
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+};
